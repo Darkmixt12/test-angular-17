a90ab4594cb5859be65bf696d8976d87b8cd0d1f
Steven Muñoz <darkmixt@gmail.com>
2023-11-21 17:55:01 -0600

    feat: defer triggers interaction,hover,immediate,timer,prefetch

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [CommonModule, HeavyLoadersSlowComponent],
+  imports: [CommonModule, HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-options.component.html',
   styles: ``
 })
--- src/app/dashboard/pages/defer-virws/defer-virws.component.ts
+++ src/app/dashboard/pages/defer-virws/defer-virws.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-view',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-virws.component.html',
   styles: ``
 })
 export default class DeferVirwsComponent {
 
+
+  
 }
