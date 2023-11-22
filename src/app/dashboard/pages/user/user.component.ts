56d8b0c5491a04700b84844765ad70b546ca0384
Steven Muñoz <darkmixt@gmail.com>
2023-11-22 12:36:36 -0600

    feat : done with all new features in angular 17

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { User } from '../../../interfaces/req-response';
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule,TitleComponent],
+  template: `
+    <app-title [title]="titleLabel()" />
+  
+    @if( user() ){
+      <section>
+        <img [srcset]="user()!.avatar"
+              class=""
+        >
+
+        <p>{{user()?.first_name}} <br> {{user()?.email}}</p>
+
+
+      </section>
+    }@else {
+      <p>Cargando usuario...</p>
+    }
+  
+  `
+ 
 })
+
+
+
+
+
 export default class UserComponent {
 
+  private route = inject( ActivatedRoute)
+  private userService = inject( UsersService );
+  public titleLabel = computed( ()=> {
+    if( this.user()) {
+      return `${ this.user()?.first_name} ${this.user()?.last_name}`
+    }else{
+      return `Informacion del usuario`
+    }
+  })
+
+
+
+
+  //public user = signal<User| undefined>(undefined)
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({id}) => this.userService.getUserById( id )) // en este caso destructuramos el params . id para solo tener que utilizar el ID en los campos
+    )
+  )
 }
--- src/app/dashboard/pages/view-transition/view-transition.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './view-transition.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template:`
+  
+  <app-title title="View Transition 1">
+  
+  <section class="flex justify-start">
+
+  <img srcset="https://picsum.photos/id/237/200/300" 
+        alt="Picsum"
+        width="200"
+        height="300" />
+
+        <div class="bg-blue-500 w-56 h-56">
+          
+        </div>
+        
+
+  </section>
+
+  `
+
 })
 export default class ViewTransitionComponent {
 
